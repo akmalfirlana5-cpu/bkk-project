35925f51d1c9dd3e0ab34f509ae4f47d86b3e404
Akmal Eka FIrlana <138485072+akmalreal@users.noreply.github.com>
2026-05-02 14:55:04 +0700

    Update DatabaseSeeder to run all setting seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        $this->call([
+            GlobalSettingSeeder::class,
+            HomepageSettingSeeder::class,
+            InfoSettingSeeder::class,
+            FaqSettingSeeder::class,
+            ContactSettingSeeder::class,
+            ProfileSettingSeeder::class,
+            SurveySeeder::class,
         ]);
     }
 }
